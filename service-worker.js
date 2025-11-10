e767d47c12967990f057eedf07462be03f07fbe5
vickie005 <mwendwavictory29@gmail.com>
2025-11-10 14:34:56 +0300

    deployed using Progressive Web App(PWA)

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+// NumSys Converter - Service Worker
+const CACHE_NAME = "numsys-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching app shell...");
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Serve from cache if available, otherwise fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
